6eebea0c3ac912066fcaf8781583664f09c0bb3b
Kamila <abdurahmanovakamila.2006@gmail.com>
2025-03-17 18:53:18 +0500

    perrmissions to tags & categories created

--- categories/views.py
+++ categories/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAdminUser
 from .models import Category
 from .serializers import CategorySerializer
 from .pagination import CategoryPagination
@@ -8,3 +10,8 @@ class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     pagination_class = CategoryPagination
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [AllowAny()]
+        return [IsAdminUser()]
--- tags/views.py
+++ tags/views.py
@@ -1,10 +1,16 @@
-from rest_framework  import viewsets
+from rest_framework  import generics
+from rest_framework.permissions import IsAdminUser, AllowAny
 from .models import Tag
 from .serializers import TagSerializer
 from .pagination import TagPagination
 
 
-class TagViewSet(viewsets.ModelViewSet):
+class TagListCreateView(generics.ListCreateAPIView):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
     pagination_class = TagPagination
+
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            return [IsAdminUser()]
+        return [AllowAny()]
