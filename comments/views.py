3390f428947f9d6c13099ef5c660bf5edc950a07
Kamila <abdurahmanovakamila.2006@gmail.com>
2025-03-18 20:13:33 +0500

    logout created

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
-
-# Create your views here.
+from rest_framework import generics
+from .models import Comment
+from .serializers import CommentSerializer
+from .pagination import CommentPagination
--- users/views.py
+++ users/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.models import User
 from rest_framework import generics
+from .serializers import RegisterSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.authentication import TokenAuthentication
-from .serializers import RegisterSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.exceptions import TokenError
+
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
@@ -12,9 +14,16 @@ class RegisterView(generics.CreateAPIView):
 
 
 class LogoutView(APIView):
-    authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
     def post(self, request):
-        request.user.auth_token.delete()
-        return Response({"message": "Successfully logged out."})
+        refresh_token = request.data.get("refresh")
+        if not refresh_token:
+            return Response({"error": "Refresh token is required"}, status=400)
+
+        try:
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response({"message": "Successfully logged out."}, status=200)
+        except TokenError:
+            return Response({"error": "Invalid token"}, status=400)
